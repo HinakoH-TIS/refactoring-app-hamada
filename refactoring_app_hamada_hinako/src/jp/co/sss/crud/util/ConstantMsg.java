7c0b58e3c109ec18b8ad58fb65c147da0fd1b63b
濱田紘苗子 <hinako-hamada@kiseki.systems>
2025-12-05 16:45:35 +0900

    add:メッセージを定数化
    狙い:保守性の向上
    日本語メッセージをConstantMsg.javaで定数化した。

---
+++ refactoring_app_hamada_hinako/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -0,0 +1,24 @@
+package jp.co.sss.crud.util;
+
+/**
+ *  設定値をまとめたクラス(メッセージ)
+ */
+public class ConstantMsg {
+
+	public static final String MSG_INPUT = "を入力してください:";
+	public static final String MSG_QUIT = "システムを終了します。";
+	public static final String MSG_NO_RESULT = "該当者はいませんでした";
+	public static final String MSG_UPDATE_COMPLETE = "を更新しました";
+	public static final String MSG_INSERT_COMPLETE = "を登録しました";
+	public static final String MSG_DELETE_COMPLETE = "を削除しました";
+
+	public static final String EMP_INFO = "社員情報";
+	public static final String MENU_NO = "メニュー番号";
+	public static final String EMP_NAME = "社員名";
+	public static final String DEPT_ID = "部署ID(1:営業部、2:経理部、3:総務部)";
+	public static final String BIRTHDAY = "生年月日(西暦年/月/日)";	
+	public static final String GENDER = "性別(0:回答しない, 1:男性, 2:女性, 9:その他)";
+	
+	
+	
+}
