7274c0484f1c03c9f9ad466f2b89a24cfe0b10ff
濱田紘苗子 <hinako-hamada@kiseki.systems>
2025-12-09 16:11:41 +0900

    fix:部署検索機能をDBControllerからEmployeeDAOに移行

--- refactoring_app_hamada_hinako/src/jp/co/sss/crud/db/EmployeeDAO.java
+++ refactoring_app_hamada_hinako/src/jp/co/sss/crud/db/EmployeeDAO.java
@@ -123,8 +123,57 @@ public class EmployeeDAO implements IEmployeeDAO {
 
 	@Override
 	public List<Employee> findByDeptId(int deptId) throws SystemErrorException {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// DBに接続
+			connection = DBManager.getConnection();
+
+			// SQL文を準備
+			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
+			sql.append(ConstantSQL.SQL_SELECT_BY_DEPT_ID);
+
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(sql.toString());
+
+			// 検索条件となる値をバインド
+			preparedStatement.setInt(1, deptId);
+
+			// SQL文を実行
+			resultSet = preparedStatement.executeQuery();
+			List<Employee> employees = new ArrayList<Employee>();
+
+			//結果をEmployeeDTOの配列ににセットして返す
+			while (resultSet.next()) {
+				Employee employee = new Employee();
+				Department department = new Department();
+				employee.setEmpId(resultSet.getInt("emp_id"));
+				employee.setEmpName(resultSet.getString("emp_name"));
+				employee.setGender(resultSet.getInt("gender"));
+				employee.setBirthday(resultSet.getString("birthday"));
+				department.setDeptName(resultSet.getString("dept_name"));
+				employee.setDepartment(department);
+				employees.add(employee);
+			}
+			return employees;
+
+		} catch (Exception e) {
+			throw new SystemErrorException();
+		} finally {
+			try {
+				// ResultSetをクローズ
+				DBManager.close(resultSet);
+				// Statementをクローズ
+				DBManager.close(preparedStatement);
+				// DBとの接続を切断
+				DBManager.close(connection);
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
