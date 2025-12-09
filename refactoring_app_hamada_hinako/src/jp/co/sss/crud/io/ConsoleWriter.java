5f367fe527ed31f4de2fc7ac8c6eb03eda27bcac
濱田紘苗子 <hinako-hamada@kiseki.systems>
2025-12-09 15:23:24 +0900

    fix:ハードコーディングされていたメッセージを定数に変更

--- refactoring_app_hamada_hinako/src/jp/co/sss/crud/io/ConsoleWriter.java
+++ refactoring_app_hamada_hinako/src/jp/co/sss/crud/io/ConsoleWriter.java
@@ -1,32 +1,52 @@
 package jp.co.sss.crud.io;
 
+import static jp.co.sss.crud.util.ConstantMsg.*;
+
 import java.util.List;
 
 import jp.co.sss.crud.dto.Employee;
-import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.util.Menu;
 
 /**
  * コンソール出力用クラス
  */
 public class ConsoleWriter {
-	
-	public static void showHeader() {
-		System.out.println("社員ID\t社員名\t性別\t生年月日\t部署名");
+
+	public static void showErrorMsg(String msg) {
+		System.out.println(msg);
 	}
-	
+
+	public static void showMenu() {
+		// メニューの表示
+		System.out.println(MENU_TITLE);
+
+		for (Menu menu : Menu.values()) {
+			System.out.println(menu.getMenuNo() + "." + menu.getMenuName());
+		}
+		System.out.print(MENU_NO + MSG_INPUT);
+	}
+
+	public static void showHeader() {
+		System.out.println(SEARCH_RESULT_HEADER);
+	}
+
 	public static void showEmployees(List<Employee> employees) {
-		
-		if(employees.isEmpty()) {
-			System.out.println(ConstantMsg.MSG_NO_RESULT);
+
+		if (employees.isEmpty()) {
+			System.out.println(MSG_NO_RESULT);
+		} else {
+			System.out.println();
+			showHeader();
+			for (Employee employee : employees) {
+				System.out.println(employee.toString());
+			}
+			System.out.println();
 		}
-		
-		System.out.println();
-		showHeader();
-		for(Employee employee : employees) {
-			System.out.println(employee.toString());
-		}
-		System.out.println();
-		
+	}
+
+	public static void showInputMessage(String inputMessage) {
+
+		System.out.println(inputMessage);
 	}
 
 }
