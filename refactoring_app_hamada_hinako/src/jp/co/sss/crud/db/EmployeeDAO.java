f75e54dd9e82667856ad2d4557b81ee7cec6eb90
濱田紘苗子 <hinako-hamada@kiseki.systems>
2025-12-10 11:30:39 +0900

    fix:社員削除機能追加

--- refactoring_app_hamada_hinako/src/jp/co/sss/crud/db/EmployeeDAO.java
+++ refactoring_app_hamada_hinako/src/jp/co/sss/crud/db/EmployeeDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,26 +15,30 @@ import jp.co.sss.crud.dto.Employee;
 import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.util.ConstantSQL;
 
+/**
+ * 社員CRUD機能用DAO
+ */
 public class EmployeeDAO implements IEmployeeDAO {
 
+	/**
+	 * 全件検索
+	 * @return 検索結果の社員リスト
+	 */
+	@Override
 	public List<Employee> findAll() throws SystemErrorException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
+		List<Employee> employees = new ArrayList<Employee>();
 
 		try {
-			// DBに接続
 			connection = DBManager.getConnection();
-
-			// ステートメントを作成
 			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_ALL_SELECT);
 
 			// SQL文を実行
 			resultSet = preparedStatement.executeQuery();
 
-			List<Employee> employees = new ArrayList<Employee>();
-
-			//結果をEmployeeDTOの配列ににセットして返す
+			//結果をEmployeeDTOの配列ににセット
 			while (resultSet.next()) {
 				Employee employee = new Employee();
 				Department department = new Department();
@@ -46,52 +51,42 @@ public class EmployeeDAO implements IEmployeeDAO {
 				employees.add(employee);
 			}
 
-			return employees;
-
-		} catch (Exception e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			throw new SystemErrorException();
 		} finally {
 
 			try {
-				// ResultSetをクローズ
 				DBManager.close(resultSet);
-				// Statementをクローズ
 				DBManager.close(preparedStatement);
-				// DBとの接続を切断
 				DBManager.close(connection);
 			} catch (SQLException e) {
-				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
-
 		}
+		return employees;
 	}
 
+	/**
+	 * 社員名検索
+	 * @return 検索結果の社員リスト
+	 */
 	@Override
 	public List<Employee> findByEmployeeName(String searchName) throws SystemErrorException {
+		List<Employee> employees = new ArrayList<Employee>();
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 
 		try {
-			// DBに接続
 			connection = DBManager.getConnection();
-
-			// SQL文を準備
 			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
 			sql.append(ConstantSQL.SQL_SELECT_BY_EMP_NAME);
-
-			// ステートメントの作成
 			preparedStatement = connection.prepareStatement(sql.toString());
-
-			// 検索条件となる値をバインド
 			preparedStatement.setString(1, "%" + searchName + "%");
 
 			resultSet = preparedStatement.executeQuery();
 
-			List<Employee> employees = new ArrayList<Employee>();
-
-			//結果をEmployeeDTOの配列ににセットして返す
+			//結果をEmployeeDTOの配列ににセット
 			while (resultSet.next()) {
 				Employee employee = new Employee();
 				Department department = new Department();
@@ -104,40 +99,37 @@ public class EmployeeDAO implements IEmployeeDAO {
 				employees.add(employee);
 			}
 
-			return employees;
-
-		} catch (Exception e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			throw new SystemErrorException();
 		} finally {
 			try {
-				// ResultSetをクローズ
+
 				DBManager.close(resultSet);
-				// Statementをクローズ
 				DBManager.close(preparedStatement);
-				// DBとの接続を切断
 				DBManager.close(connection);
 			} catch (SQLException e) {
-				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
 		}
+		return employees;
+
 	}
 
+	/**
+	 * 部署ID検索
+	 * @return 検索結果の社員リスト
+	 */
 	@Override
 	public List<Employee> findByDeptId(int deptId) throws SystemErrorException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
+		List<Employee> employees = new ArrayList<Employee>();
 
 		try {
-			// DBに接続
 			connection = DBManager.getConnection();
-
-			// SQL文を準備
 			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
 			sql.append(ConstantSQL.SQL_SELECT_BY_DEPT_ID);
-
-			// ステートメントの作成
 			preparedStatement = connection.prepareStatement(sql.toString());
 
 			// 検索条件となる値をバインド
@@ -145,7 +137,6 @@ public class EmployeeDAO implements IEmployeeDAO {
 
 			// SQL文を実行
 			resultSet = preparedStatement.executeQuery();
-			List<Employee> employees = new ArrayList<Employee>();
 
 			//結果をEmployeeDTOの配列ににセットして返す
 			while (resultSet.next()) {
@@ -159,33 +150,31 @@ public class EmployeeDAO implements IEmployeeDAO {
 				employee.setDepartment(department);
 				employees.add(employee);
 			}
-			return employees;
 
-		} catch (Exception e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			throw new SystemErrorException();
 		} finally {
 			try {
-				// ResultSetをクローズ
 				DBManager.close(resultSet);
-				// Statementをクローズ
 				DBManager.close(preparedStatement);
-				// DBとの接続を切断
 				DBManager.close(connection);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+		return employees;
+
 	}
 
+	/**
+	 * 社員登録
+	 */
 	@Override
 	public void insert(Employee employee) throws SystemErrorException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		try {
-			// DBに接続
 			connection = DBManager.getConnection();
-
-			// ステートメントを作成
 			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_INSERT);
 
 			// 入力値をバインド
@@ -197,30 +186,30 @@ public class EmployeeDAO implements IEmployeeDAO {
 
 			// SQL文を実行
 			preparedStatement.executeUpdate();
-		} catch (Exception e) {
+
+		} catch (SQLException | ClassNotFoundException | ParseException e) {
 			throw new SystemErrorException();
 		} finally {
-
 			try {
 				DBManager.close(preparedStatement);
 				DBManager.close(connection);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-
 		}
 	}
 
+	/**
+	 * 社員更新
+	 * @return 更新件数（失敗時は0）
+	 */
 	@Override
 	public Integer update(Employee employee) throws SystemErrorException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
-		
-		try {
-			// DBに接続
-			connection = DBManager.getConnection();
 
-			// ステートメントを作成
+		try {
+			connection = DBManager.getConnection();
 			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_UPDATE);
 
 			// 入力値をバインド
@@ -232,8 +221,8 @@ public class EmployeeDAO implements IEmployeeDAO {
 			preparedStatement.setInt(5, employee.getEmpId());
 
 			return preparedStatement.executeUpdate();
-	
-		} catch (Exception e) {
+
+		} catch (SQLException | ClassNotFoundException | ParseException e) {
 			throw new SystemErrorException();
 		} finally {
 
@@ -243,31 +232,59 @@ public class EmployeeDAO implements IEmployeeDAO {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-
 		}
 	}
 
+	
+	/**
+	 * 社員削除
+	 * @return 削除件数（失敗時は0）
+	 */
 	@Override
 	public Integer delete(Integer empId) throws SystemErrorException {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+
+			connection = DBManager.getConnection();
+			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_DELETE);
+
+			// 社員IDをバインド
+			preparedStatement.setInt(1, empId);
+
+			// SQL文の実行(失敗時は戻り値0)
+			return preparedStatement.executeUpdate();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			throw new SystemErrorException();
+
+		} finally {
+			try {
+				DBManager.close(preparedStatement);
+				DBManager.close(connection);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
+	/**
+	 * 社員ID検索
+	 * @return 検索結果の社員リスト
+	 */
 	@Override
 	public List<Employee> findByEmpId(int empId) throws SystemErrorException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
+		List<Employee> employees = new ArrayList<Employee>();
 
 		try {
-			// DBに接続
-			connection = DBManager.getConnection();
 
-			// SQL文を準備
+			connection = DBManager.getConnection();
 			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
 			sql.append(ConstantSQL.SQL_SELECT_BY_EMP_ID);
-
-			// ステートメントの作成
 			preparedStatement = connection.prepareStatement(sql.toString());
 
 			// 検索条件となる値をバインド
@@ -275,7 +292,6 @@ public class EmployeeDAO implements IEmployeeDAO {
 
 			// SQL文を実行
 			resultSet = preparedStatement.executeQuery();
-			List<Employee> employees = new ArrayList<Employee>();
 
 			//結果をEmployeeDTOの配列ににセットして返す
 			while (resultSet.next()) {
@@ -289,22 +305,20 @@ public class EmployeeDAO implements IEmployeeDAO {
 				employee.setDepartment(department);
 				employees.add(employee);
 			}
-			return employees;
 
-		} catch (Exception e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			throw new SystemErrorException();
 		} finally {
 			try {
-				// ResultSetをクローズ
 				DBManager.close(resultSet);
-				// Statementをクローズ
 				DBManager.close(preparedStatement);
-				// DBとの接続を切断
 				DBManager.close(connection);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+
+		return employees;
 	}
 
 }
